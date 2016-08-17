516d57d550651c19a32c5abac54463cdb56026bd
jsonmartin <jason.carter.martin@gmail.com>
2016-08-16 20:35:59 -0700

    Cleaned up some unused scripts

--- js/custom.js
+++ js/custom.js
@@ -753,8 +753,8 @@ $(window).load(function() {
     |--------------------------------------------------------------------------
     */ 
     $('#status').fadeOut(); // will first fade out the loading animation
-    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-    $('body').delay(350).css({'overflow':'visible'});
+    $('#preloader').delay(200).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    $('body').delay(200).css({'overflow':'visible'});
 
     /*
     |--------------------------------------------------------------------------
