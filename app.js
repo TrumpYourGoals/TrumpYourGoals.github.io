f0194e2348a8b682e6da05925b9812c72487f1bc
jsonmartin <jason.carter.martin@gmail.com>
2016-09-13 21:49:00 -0700

    Switched to live key

--- app.js
+++ app.js
@@ -43,36 +43,6 @@ angular.module('trump', ['ui.bootstrap', 'backand', '720kb.socialshare'])
         stripe_token_id: tokenID
       }
     });
-
-    // return $http ({
-    //   method: 'POST',
-    //   url: Backand.getApiUrl() + '/1/objects/action/goals/1?name=createStripeCustomer',
-    //   data: {
-    //     // email: 'asdf@fdsza.com',
-    //     // stripe_token_id: 'tok_18l8bM273fOFEuQM2Kok0BX0'
-    //     stripe_token_id: token,
-    //     email: $scope.email
-    //   }
-    // });
-    // $http({
-    //   method: 'GET',
-    //   url: Backand.getApiUrl() + '/1/objects/action/goals/1?name=createStripeCustomer',
-    //   params:{
-    //     parameters:{
-    //       stripe_token_id: token,
-    //       email: $scope.email
-    //     }
-    //   }
-    // }).then(function () {
-    //   console.log('success creating customer');
-    // }).catch(function (err) {
-    //     if (err.type && /^Stripe/.test(err.type)) {
-    //       console.error('Stripe error: ', err);
-    //     }
-    //     else {
-    //       console.error('Other error occurred, possibly with your API', err.message);
-    //     }
-    //   });
   };
 
   var handler = null;
@@ -80,7 +50,7 @@ angular.module('trump', ['ui.bootstrap', 'backand', '720kb.socialshare'])
   $scope.getCardDetails = function(email, recipient) {
     if (!handler) {
       handler = handler || StripeCheckout.configure({
-          key: 'pk_test_xi8hyOSW2WZaKX1LZ2mgdMAH', //pk_live_R2hcONSAEfIcn8vC18Xiv7gE
+          key: 'pk_live_R2hcONSAEfIcn8vC18Xiv7gE', //'pk_test_xi8hyOSW2WZaKX1LZ2mgdMAH', //'pk_live_R2hcONSAEfIcn8vC18Xiv7gE'
           locale: 'auto',
           token: function(token) {
             //debugger;
@@ -359,143 +329,4 @@ angular.module('trump', ['ui.bootstrap', 'backand', '720kb.socialshare'])
   $scope.popup2 = {
     opened: false
   };
-
-  // var tomorrow = new Date();
-  // tomorrow.setDate(tomorrow.getDate() + 1);
-  // var afterTomorrow = new Date();
-  // afterTomorrow.setDate(tomorrow.getDate() + 1);
-  // $scope.events = [
-  //   {
-  //     date: tomorrow,
-  //     status: 'full'
-  //   },
-  //   {
-  //     date: afterTomorrow,
-  //     status: 'partially'
-  //   }
-  // ];
-  //
-  // function getDayClass(data) {
-  //   var date = data.date,
-  //     mode = data.mode;
-  //   if (mode === 'day') {
-  //     var dayToCheck = new Date(date).setHours(0,0,0,0);
-  //
-  //     for (var i = 0; i < $scope.events.length; i++) {
-  //       var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
-  //
-  //       if (dayToCheck === currentDay) {
-  //         return $scope.events[i].status;
-  //       }
-  //     }
-  //   }
-  //
-  //   return '';
-  // }
-
-
-
-
-  // $scope.today = function() {
-  //   $scope.dt = new Date();
-  // };
-  // $scope.today();
-  //
-  // $scope.clear = function() {
-  //   $scope.dt = null;
-  // };
-  //
-  // $scope.inlineOptions = {
-  //   customClass: getDayClass,
-  //   minDate: new Date(),
-  //   showWeeks: true
-  // };
-  //
-  // $scope.dateOptions = {
-  //   dateDisabled: disabled,
-  //   formatYear: 'yy',
-  //   maxDate: new Date(2020, 5, 22),
-  //   minDate: new Date(),
-  //   startingDay: 1
-  // };
-  //
-  // // Disable weekend selection
-  // function disabled(data) {
-  //   var date = data.date,
-  //     mode = data.mode;
-  //   return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
-  // }
-  //
-  // $scope.toggleMin = function() {
-  //   $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
-  //   $scope.dateOptions.minDate = $scope.inlineOptions.minDate;
-  // };
-  //
-  // $scope.toggleMin();
-  //
-  // $scope.open1 = function() {
-  //   $scope.popup1.opened = true;
-  // };
-  //
-  // $scope.open2 = function() {
-  //   $scope.popup2.opened = true;
-  // };
-  //
-  // $scope.setDate = function(year, month, day) {
-  //   $scope.dt = new Date(year, month, day);
-  // };
-  //
-  // $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-  // $scope.format = $scope.formats[0];
-  // $scope.altInputFormats = ['M!/d!/yyyy'];
-  //
-  // $scope.popup1 = {
-  //   opened: false
-  // };
-  //
-  // $scope.popup2 = {
-  //   opened: false
-  // };
-  //
-  // var tomorrow = new Date();
-  // tomorrow.setDate(tomorrow.getDate() + 1);
-  // var afterTomorrow = new Date();
-  // afterTomorrow.setDate(tomorrow.getDate() + 1);
-  // $scope.events = [
-  //   {
-  //     date: tomorrow,
-  //     status: 'full'
-  //   },
-  //   {
-  //     date: afterTomorrow,
-  //     status: 'partially'
-  //   }
-  // ];
-  //
-  // function getDayClass(data) {
-  //   var date = data.date,
-  //     mode = data.mode;
-  //   if (mode === 'day') {
-  //     var dayToCheck = new Date(date).setHours(0,0,0,0);
-  //
-  //     for (var i = 0; i < $scope.events.length; i++) {
-  //       var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
-  //
-  //       if (dayToCheck === currentDay) {
-  //         return $scope.events[i].status;
-  //       }
-  //     }
-  //   }
-  //
-  //   return '';
-  // }
-
-
-
 });
-
-Date.prototype.withoutTime = function () {
-  var d = new Date(this);
-  d.setHours(0, 0, 0, 0, 0);
-  return d
-};
